4958ea8066e8125f68b6474ca8f6d10bf40a40ca
Ermal ALIRAJ <ermal.aliraj@gmail.com>
2022-03-10 11:06:53 +0100

    new version

--- CssImgJsVfolder/js/main.js
+++ CssImgJsVfolder/js/main.js
@@ -163,40 +163,50 @@
   });
 
   /**
+   * Preloader
+   */
+  let preloader = select("#preloader");
+  if (preloader) {
+    window.addEventListener("load", () => {
+      setTimeout(preloader.remove(), 155000);
+      clearTimeout();
+    });
+  }
+  /**
    * Clients Slider
    */
-  new Swiper(".clients-slider", {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60,
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80,
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120,
-      },
-    },
-  });
+  // new Swiper(".clients-slider", {
+  //   speed: 100,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 1000,
+  //     disableOnInteraction: false,
+  //   },
+  //   slidesPerView: "auto",
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 2,
+  //       spaceBetween: 40,
+  //     },
+  //     480: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 60,
+  //     },
+  //     640: {
+  //       slidesPerView: 4,
+  //       spaceBetween: 80,
+  //     },
+  //     992: {
+  //       slidesPerView: 6,
+  //       spaceBetween: 120,
+  //     },
+  //   },
+  // });
 
   /**
    * Porfolio isotope and filter
@@ -234,53 +244,53 @@
   /**
    * Initiate portfolio lightbox
    */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfokio-lightbox",
-  });
+  // const portfolioLightbox = GLightbox({
+  //   selector: ".portfokio-lightbox",
+  // });
 
   /**
    * Portfolio details slider
    */
-  new Swiper(".portfolio-details-slider", {
-    speed: 400,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
+  // new Swiper(".portfolio-details-slider", {
+  //   speed: 200,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  // });
 
   /**
    * Tech testimonial slider
    */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40,
-      },
-
-      1200: {
-        slidesPerView: 3,
-      },
-    },
-  });
+  // new Swiper(".testimonials-slider", {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   slidesPerView: "auto",
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 40,
+  //     },
+  //
+  //     1200: {
+  //       slidesPerView: 3,
+  //     },
+  //   },
+  // });
 
   /**
    * Animation on scroll
